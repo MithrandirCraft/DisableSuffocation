7ac6b701232727576c1c98a9d5266e0dbe67bb4f
Dylan Calaf <dylancalaflatham@gmail.com>
2021-03-07 00:00:07 +0100

    Account for barrier

--- src/main/java/es/mithrandircraft/disablesuffocation/DisableSuffocation.java
+++ src/main/java/es/mithrandircraft/disablesuffocation/DisableSuffocation.java
@@ -1,6 +1,8 @@
 package es.mithrandircraft.disablesuffocation;
 
-import org.bukkit.WorldBorder;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,23 +11,55 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DisableSuffocation extends JavaPlugin implements Listener {
 
+    double overworldWorldborderRadius;
+    double netherWorldborderRadius;
+    double endWorldborderRadius;
+
     @Override
     public void onEnable() {
         //Events:
         getServer().getPluginManager().registerEvents(this, this);
+
+        overworldWorldborderRadius = Bukkit.getWorlds().get(0).getWorldBorder().getSize() / 2d;
+        netherWorldborderRadius = Bukkit.getWorlds().get(1).getWorldBorder().getSize() / 2d;
+        endWorldborderRadius = Bukkit.getWorlds().get(2).getWorldBorder().getSize() / 2d;
     }
 
     //Block suffocation
     @EventHandler
     public void EntityDamageEvent(EntityDamageEvent ev)
-    {
-        if(ev.getEntityType() == EntityType.PLAYER && ev.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION)
+    {                                                                                                               //Outside border exclusion
+        if(ev.getEntityType() == EntityType.PLAYER && ev.getCause() == EntityDamageEvent.DamageCause.SUFFOCATION && CheckWithinWorldborder(ev.getEntity().getWorld(), ev.getEntity().getLocation()))
         {
-            //Outside border exclusion
-            double worldDiameter = ev.getEntity().getWorld().getWorldBorder().getSize()/2;
-            if(ev.getEntity().getLocation().getBlockX() > worldDiameter || ev.getEntity().getLocation().getBlockZ() > worldDiameter ||
-               ev.getEntity().getLocation().getBlockX() < -worldDiameter || ev.getEntity().getLocation().getBlockZ() < -worldDiameter)
-                    ev.setCancelled(true);
+            ev.setCancelled(true);
         }
+    }
+
+    public Boolean CheckWithinWorldborder(World world, Location location)
+    {
+        switch(world.getEnvironment())
+        {
+            case NORMAL:
+            {
+                return CheckWithinRadius(overworldWorldborderRadius, location);
+            }
+            case NETHER:
+            {
+                return CheckWithinRadius(netherWorldborderRadius, location);
+            }
+            case THE_END:
+            {
+                return CheckWithinRadius(endWorldborderRadius, location);
+            }
+            default:
+            {
+                return true;
+            }
+        }
+    }
+
+    public Boolean CheckWithinRadius(double radius, Location location)
+    {
+        return !(location.getBlockX() > radius) && !(location.getBlockZ() > radius) && !(location.getBlockX() < -radius) && !(location.getBlockZ() < -radius);
     }
 }
